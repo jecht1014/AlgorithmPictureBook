e3af15311632bdb00ba1bd78a7d9e62374dcb285
JunjiSakurai <s153049@matsu.shimane-u.ac.jp>
2019-08-30 06:28:02 +0900

    change time.time() function

--- AlgorithmPictureBook/main.py
+++ AlgorithmPictureBook/main.py
@@ -1,3 +1,7 @@
 import sort
+import time
+
 l = [5, 3, 4, 7, 2, 8, 6, 9, 1]
-l = sort.choice_sort(l, True)
+start_time = time.time()
+sorted_list = sort.selection_sort(l, True)
+print('time:{0:.3f}'.format(time.time()-start_time))
--- AlgorithmPictureBook/sort.py
+++ AlgorithmPictureBook/sort.py
@@ -1,16 +1,15 @@
-l = [5, 3, 4, 7, 2, 8, 6, 9, 1]
-
 #選択ソート
-for i in range(len(l)-1):
-	min = l[i]
-	argmin = i
-	for j in range(i+1, len(l)):
-		if(min > l[j]):
-			min = l[j]
-			argmin = j
-			
-	tmp = l[i]
-	l[i] = l[argmin]
-	l[argmin] = tmp
-	
-	print('loop:{0} list:{1}'.format(i, l))
+def selection_sort(sort_list, list_print = False):
+	for i in range(len(sort_list)-1):
+		min = sort_list[i]
+		argmin = i
+		for j in range(i+1, len(sort_list)):
+			if(min > sort_list[j]):
+				min = sort_list[j]
+				argmin = j
+				
+		tmp = sort_list[i]
+		sort_list[i] = sort_list[argmin]
+		sort_list[argmin] = tmp
+		if list_print:
+			print('loop:{0} list:{1}'.format(i, sort_list))
