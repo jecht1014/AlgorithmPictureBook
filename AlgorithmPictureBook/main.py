a7a190065abfc8e7aa2ab97b3d59fbac50f98bcb
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-08-30 22:45:12 +0900

    実行時間の比較を行えるように変更

--- AlgorithmPictureBook/main.py
+++ AlgorithmPictureBook/main.py
@@ -1,7 +1,31 @@
 import sort
 import time
+import random
 
-l = [5, 3, 4, 7, 2, 8, 6, 9, 1]
+#再帰条件の変更
+import sys
+sys.setrecursionlimit(100000)
+
+l = list(range(1000))
+random.shuffle(l)
+
 start_time = time.time()
-sorted_list = sort.selection_sort(l, True)
+sorted_list = sort.bubble_sort(l)
+print('time:{0:.3f}'.format(time.time()-start_time))
+
+start_time = time.time()
+sorted_list = sort.selection_sort(l)
+print('time:{0:.3f}'.format(time.time()-start_time))
+
+start_time = time.time()
+sorted_list = sort.insertion_sort(l)
+print('time:{0:.3f}'.format(time.time()-start_time))
+
+start_time = time.time()
+sorted_list = sort.heap_sort(l)
+print('time:{0:.3f}'.format(time.time()-start_time))
+
+start_time = time.time()
+sorted_list = sort.merge_sort(l)
+#print(sorted_list)
 print('time:{0:.3f}'.format(time.time()-start_time))
