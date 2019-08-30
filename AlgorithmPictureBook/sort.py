d5cfcfef5183511c62c1c67340497e2445835d3c
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-08-30 13:04:25 +0900

    bubble_sortの追加

--- AlgorithmPictureBook/sort.py
+++ AlgorithmPictureBook/sort.py
@@ -1,3 +1,21 @@
+def replace_list(l, i, j):
+	tmp = l[i]
+	l[i] = l[j]
+	l[j] = tmp
+	return l
+
+#バブルソート
+def bubble_sort(sort_list, list_print = False):
+	for i in range(len(sort_list)-1):
+		for j in range(len(sort_list) - 1 - i):
+			if sort_list[j] > sort_list[j+1]:
+				sort_list = replace_list(sort_list, j, j+1)
+		
+		if list_print:
+			print('loop:{0} list:{1}'.format(i, sort_list))
+
+	return replace_list
+
 #選択ソート
 def selection_sort(sort_list, list_print = False):
 	for i in range(len(sort_list)-1):
@@ -7,9 +25,7 @@ def selection_sort(sort_list, list_print = False):
 			if(min > sort_list[j]):
 				min = sort_list[j]
 				argmin = j
-				
-		tmp = sort_list[i]
-		sort_list[i] = sort_list[argmin]
-		sort_list[argmin] = tmp
+		
+		sort_list = replace_list(sort_list, i, argmin)
 		if list_print:
 			print('loop:{0} list:{1}'.format(i, sort_list))
