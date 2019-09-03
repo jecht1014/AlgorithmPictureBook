ac510e11d54655a0f67b838c43983e6eec1653ba
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-09-04 02:04:19 +0900

    'ディレクトリの追加'

---
+++ GaussianProcessAndMachineLearning/1_linear_regression/linear_regression.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def simple_regression(xn, yn):
+    n = xn.shape[0]
+    a = (np.sum(np.square(xn)) * np.sum(yn) - np.sum(xn) * np.sum(xn * yn)) / (n * np.sum(np.square(xn)) - np.square(np.sum(xn)))
+    b = (n * np.sum(xn * yn) - np.sum(xn) * np.sum(yn)) / (n * np.sum(np.square(xn)) - np.square(np.sum(xn)))
+
+    return a, b
+
+def multiple_regression(X, y):
+    w = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y)
+    return w
+
+def linear_regression(phi, y):
+    w = np.dot(np.dot(np.linalg.inv(np.dot(phi.T, phi)), phi.T), y)
+    return w
