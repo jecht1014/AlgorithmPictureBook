0bfbd5350b4ebcf9dfd2314b67945bf7e517b8e5
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-09-06 16:19:40 +0900

    'mainの修正'

--- GaussianProcessAndMachineLearning/2_gaussian_distribution/main.py
+++ GaussianProcessAndMachineLearning/2_gaussian_distribution/main.py
@@ -12,7 +12,7 @@ def gaussian_probability_density_plot(mu = 0, sigma2 = 1):
 
 def box_muller_plot(n, mu = 0, sigma2 = 1):
     y = np.ones(n)
-    x = np.array([box_muller() for i in range(n)])
+    x = np.array([box_muller(mu = mu, sigma2 = sigma2) for i in range(n)])
 
     plt.hist(x)
     plt.show()
