4450c62ae00f422dab989c4b8ea0a97d80dedc4b
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-09-06 16:18:28 +0900

    '正規乱数の生成とプロットの作成'

--- GaussianProcessAndMachineLearning/2_gaussian_distribution/gaussian_distribution.py
+++ GaussianProcessAndMachineLearning/2_gaussian_distribution/gaussian_distribution.py
@@ -1,3 +1,9 @@
 import numpy as np
+import random
+import math
+
 def gaussian_probability_density(x, mu = 0, sigma2 = 1):
-   return (1 / np.sqrt(2*np.pi) * sigma2) * np.exp(-np.power(x - mu, 2) / 2 * sigma2)
+    return (1 / np.sqrt(2*np.pi) * sigma2) * np.exp(-np.power(x - mu, 2) / 2 * sigma2)
+
+def box_muller(mu = 0, sigma2 = 1):
+    return mu + sigma2 * (math.sqrt(-2 * math.log(random.random())) * math.sin(2 * math.pi * random.random()))
--- GaussianProcessAndMachineLearning/2_gaussian_distribution/main.py
+++ GaussianProcessAndMachineLearning/2_gaussian_distribution/main.py
@@ -10,4 +10,14 @@ def gaussian_probability_density_plot(mu = 0, sigma2 = 1):
     plt.plot(x, y)
     plt.show()
 
+def box_muller_plot(n, mu = 0, sigma2 = 1):
+    y = np.ones(n)
+    x = np.array([box_muller() for i in range(n)])
+
+    plt.hist(x)
+    plt.show()
+
+'''
 gaussian_probability_density_plot()
+box_muller_plot(10000)
+'''
