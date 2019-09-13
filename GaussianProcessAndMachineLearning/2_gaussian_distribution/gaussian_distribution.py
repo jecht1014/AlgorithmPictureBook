e7237aefd7d07884a4fbbfc7627ffe37cb63ccee
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-09-13 21:12:30 +0900

    '多変量ガウス分布の確率密度関数の作成'

--- GaussianProcessAndMachineLearning/2_gaussian_distribution/gaussian_distribution.py
+++ GaussianProcessAndMachineLearning/2_gaussian_distribution/gaussian_distribution.py
@@ -7,3 +7,8 @@ def gaussian_probability_density(x, mu = 0, sigma2 = 1):
 
 def box_muller(mu = 0, sigma2 = 1):
     return mu + sigma2 * (math.sqrt(-2 * math.log(random.random())) * math.sin(2 * math.pi * random.random()))
+
+# mu:xの平均値ベクトル, sigma:共分散行列
+def multivariate_gaussian_probability_density(x, mu, sigma):
+    normalization_constant = 1 / (np.power(np.sqrt(2 * np.pi), sigma.shape[0]) * np.sqrt(np.linalg.det(sigma)))
+    return normalization_constant * np.exp(-1 / 2 * np.dot(np.dot((x - mu).T, np.linalg.inv(sigma)), (x - mu)))
