f68766fe344878cb9249e0cb82e2814cba8bd4d5
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-09-22 00:24:54 +0900

    '新パーリンノイズの追加'

--- shumi/perlin_noise.py
+++ shumi/perlin_noise.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def waveletf(x, a = None):
+def old_waveletf(x, a = None):
     if a == None:
         l = np.random.normal(0, 3, 1) * x
     else:
@@ -9,10 +9,18 @@ def waveletf(x, a = None):
     y = (1 -3 * np.power(x, 2) + 2 * np.abs(np.power(x, 3))) * l
     return y
 
+def new_waveletf(x, a = None):
+    if a == None:
+        l = np.random.normal(0, 3, 1) * x
+    else:
+        l = a * x
+    y = (1 - (6 * np.abs(np.power(x, 5)) -15 * np.power(x, 4) + 10 * np.abs(np.power(x, 3)))) * l
+    return y
+
 def perlin_noise_plot():
     x = np.linspace(-1, 1, 51)
-    y_left = waveletf(x)
-    y_right = waveletf(x)
+    y_left = new_waveletf(x)
+    y_right = new_waveletf(x)
 
     plt.plot(np.linspace(-1, 1, 51), y_left)
     plt.plot(np.linspace(0, 2, 51), y_right)
@@ -23,9 +31,9 @@ def set_perlin_noise(n = 5, plot_judge = False):
     x = np.linspace(-1, 1, 51)
     y = np.array([])
     
-    y_left = waveletf(x)
+    y_left = new_waveletf(x)
     for _ in range(n):
-        y_right = waveletf(x)
+        y_right = new_waveletf(x)
         y = np.hstack([y, y_left[26:] + y_right[1:26]])
         y_left = y_right
     
@@ -36,11 +44,12 @@ def set_perlin_noise(n = 5, plot_judge = False):
 
     return y
 
-def plot_waceletf():
+def plot_waveletf():
     x = np.linspace(-1, 1, 50)
-    parlin_noise = waveletf(x)
+    parlin_noise = new_waveletf(x)
 
     plt.plot(x, parlin_noise)
     plt.show()
 
-ra = set_perlin_noise(10, True)
+a = set_perlin_noise(10, True)
+plot_waveletf()
