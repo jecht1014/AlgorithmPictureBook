adb9edb4af7764b07f4cec7840d46bbc33bf818c
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-09-22 03:56:50 +0900

    '2dパーリンノイズのサンプル作成'

--- shumi/perlin_noise.py
+++ shumi/perlin_noise.py
@@ -22,7 +22,7 @@ def new_waveletf(x, a = None):
     y = (1 - (6 * np.abs(np.power(x, 5)) -15 * np.power(x, 4) + 10 * np.abs(np.power(x, 3)))) * l
     return y
 
-def set_perlin_noise(n = 5, plot_judge = False):
+def set_perlin_noise1d(n = 5, plot_judge = False):
     x = np.linspace(-1, 1, 51)
     y = np.array([])
     
@@ -39,16 +39,11 @@ def set_perlin_noise(n = 5, plot_judge = False):
 
     return y
 
-def waveletf2d(a = True):
-    x = np.linspace(-1, 1, 51)
-    y = np.linspace(-1, 1, 51)
-    x, y = np.meshgrid(x, y)
-
+def waveletf2d(x, y, a = True):
     c1 = new_waveletf(x, a)
     c2 = new_waveletf(y, a)
 
     z = c1 * c2 * (np.random.normal(0, 3, 1)*x + np.random.normal(0, 3, 1)*y)
-
     return z
 
 def plot_waveletf():
@@ -58,7 +53,7 @@ def plot_waveletf():
     plt.plot(x, parlin_noise)
     plt.show()
 
-def perlin_noise_plot():
+def plot_perlin_noise1d():
     x = np.linspace(-1, 1, 51)
     y_left = new_waveletf(x)
     y_right = new_waveletf(x)
@@ -68,18 +63,48 @@ def perlin_noise_plot():
     plt.plot(np.linspace(0, 1, 26), y_left[25:] + y_right[:26])
     plt.show()
 
-def plot_wavelet3d():
+def plot_wavelet2d():
     u = np.linspace(-1, 1, 51)
     v = np.linspace(-1, 1, 51)
     u, v = np.meshgrid(u, v)
-    y = waveletf2d()
+    y = waveletf2d(u, v)
 
+    plt.pcolormesh(u, v, y, cmap='Greys')
+    plt.colorbar(orientation="vertical")
+    '''
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     surf = ax.plot_surface(u, v, y, cmap='bwr', linewidth=0)
     fig.colorbar(surf)
     ax.set_title("Surface Plot")
+    '''
+    plt.show()
+
+def plot_perlin_noise2d():
+    sep = 51
+    u1 = np.linspace(0, 1, sep)
+    v1 = np.linspace(0, 1, sep)
+    u1, v1 = np.meshgrid(u1, v1)
+    y1 = waveletf2d(u1, v1)
+
+    u2 = np.linspace(-1, 0, sep)
+    v2 = np.linspace(0, 1, sep)
+    u2, v2 = np.meshgrid(u2, v2)
+    y2 = waveletf2d(u2, v2)
+
+    u3 = np.linspace(0, 1, sep)
+    v3 = np.linspace(-1, 0, sep)
+    u3, v3 = np.meshgrid(u3, v3)
+    y3 = waveletf2d(u3, v3)
+
+    u4 = np.linspace(-1, 0, sep)
+    v4 = np.linspace(-1, 0, sep)
+    u4, v4 = np.meshgrid(u4, v4)
+    y4 = waveletf2d(u4, v4)
+
+    plt.pcolormesh(u1, v1, y1+y2+y3+y4, cmap='Greys')
+    plt.colorbar(orientation="vertical")
     plt.show()
 
 #a = set_perlin_noise(10, True)
-plot_wavelet3d()
+plot_perlin_noise2d()
