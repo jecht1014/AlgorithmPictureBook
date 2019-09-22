8cb7374cdac36986ca302fcc8f92df161d16ae17
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-09-22 17:21:00 +0900

    'パーリンノイズ2dの3dplotの追加と、axのaの選択肢の追加'

--- shumi/perlin_noise.py
+++ shumi/perlin_noise.py
@@ -2,6 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+def a_function(direction_num = None):
+    if direction_num == None:
+        a = np.random.normal(0, 3, 1)
+    elif direction_num == 4:
+        a = np.random.rand()
+        if a < 0.25:
+            a = 1 / 1000
+        elif a < 0.5:
+            a = 1000
+        elif a < 0.75:
+            a = -1 / 1000
+        else:
+            a = -1000
+    elif direction_num == 8:
+        a = np.random.rand()
+        if a < 0.125:
+            a = 1 / 1000
+        elif a < 0.25:
+            a = 1
+        elif a < 0.375:
+            a = 1000
+        elif a < 0.5:
+            a = -1
+        elif a < 0.625:
+            a = -1 / 1000
+        elif a < 0.75:
+            a = 1
+        elif a < 0.875:
+            a = -1000
+        else:
+            a = -1
+    return a
+
 def old_waveletf(x, a = None):
     if a == None:
         l = np.random.normal(0, 3, 1) * x
@@ -39,15 +72,14 @@ def set_perlin_noise1d(n = 5, plot_judge = False):
 
     return y
 
-def waveletf2d(x, y, a = True):
+def waveletf2d(x, y, direction_num = None, a = True):
     c1 = new_waveletf(x, a)
     c2 = new_waveletf(y, a)
 
-    z = c1 * c2 * (np.random.normal(0, 3, 1)*x + np.random.normal(0, 3, 1)*y)
+    z = c1 * c2 * (a_function(direction_num = direction_num)*x + a_function(direction_num = direction_num)*y)
     return z
 
-def set_perlin_noise2d(n = 2, plot_judge = False):
-    sep = 51
+def set_perlin_noise2d(n, sep = 51, direction_num = None, plot_judge = False):
     u = np.linspace(-1, 1, sep)
     v = np.linspace(-1, 1, sep)
     u, v = np.meshgrid(u, v)
@@ -55,18 +87,44 @@ def set_perlin_noise2d(n = 2, plot_judge = False):
 
     for i in range(n):
         for j in range(n):
-            z[i*int(sep/2):i*int(sep/2)+sep, j*int(sep/2):j*int(sep/2)+sep] += waveletf2d(u, v)
-
-            if i % 50 == 0 and j % 50 == 0:
-                print(i, j)
-
+            z[i*int(sep/2):i*int(sep/2)+sep, j*int(sep/2):j*int(sep/2)+sep] += waveletf2d(u, v, direction_num = direction_num)
+ 
     x = np.linspace(0, n-1, int(sep/2) * (n-1) + 1)
     y = np.linspace(0, n-1, int(sep/2) * (n-1) + 1)
     x, y = np.meshgrid(x, y)
 
-    plt.pcolormesh(x, y, z[int(sep/2):int(sep/2) * n + 1, int(sep/2):int(sep/2) * n + 1], cmap='Greys')
-    plt.colorbar(orientation="vertical")
-    plt.show()
+    if plot_judge:
+        plt.pcolormesh(x, y, z[int(sep/2):int(sep/2) * n + 1, int(sep/2):int(sep/2) * n + 1], cmap='Greys')
+        plt.colorbar(orientation="vertical")
+        plt.show()
+    return z[int(sep/2):int(sep/2) * n + 1, int(sep/2):int(sep/2) * n + 1]
+
+def fractal_perlin_noise2d(fractal_num = 7, direction_num = None, plot_way = '3d'):
+    sep = 16 * (2**fractal_num)
+    x = np.linspace(0, 1, int(sep/2)+1)
+    y = np.linspace(0, 1, int(sep/2)+1)
+    x, y = np.meshgrid(x, y)
+    for i in range(fractal_num):
+        if i == 0:
+            z = set_perlin_noise2d(n = 2 ** (i+1), sep = int(sep / (2 ** i)) +1, direction_num = None)
+        else:
+            z += set_perlin_noise2d(n = 2 ** (i+1), sep = int(sep / (2 ** i)) +1, direction_num = None)[:z.shape[0], :z.shape[1]]
+
+    z = z / fractal_num
+
+    if plot_way == '2d':
+        plt.pcolormesh(x, y, z, cmap='Greys')
+        plt.colorbar(orientation="vertical")
+        plt.show()
+    elif plot_way == '3d':
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(x, y, z, cmap='bwr', linewidth=0)
+        fig.colorbar(surf)
+        ax.set_title("Surface Plot")
+        plt.show()
+
+    return z
 
 def plot_waveletf():
     x = np.linspace(-1, 1, 50)
@@ -129,4 +187,5 @@ def plot_perlin_noise2d():
     plt.show()
 
 #a = set_perlin_noise(10, True)
-set_perlin_noise2d(100)
+#set_perlin_noise2d(5, direction_num=8, plot_judge = True)
+a = fractal_perlin_noise2d()
