a0c40c43656c8a678e2124f3925a9c383f6f337f
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-10-30 09:31:26 +0900

    ガウス過程回帰の実装

--- GaussianProcessAndMachineLearning/gaussian_def/gaussian_process.py
+++ GaussianProcessAndMachineLearning/gaussian_def/gaussian_process.py
@@ -32,7 +32,23 @@ def periodic_kernel(x1, x2, theta1 = 1, theta2 = 1):
     return np.exp(theta1 * np.cos(np.abs(x1-x2) / theta2))
 
 # カーネルで共分散行列の作製
-def make_k(x, kernel):
+def make_k(x, sigma2, kernel):
     x2 = np.tile(x.T, (x.shape[0], 1))
-    k = kernel(x, x2)
+    k = kernel(x, x2) + sigma2 * np.eye(x2.shape[0])
     return k
+
+# ガウス過程回帰
+def gaussian_process(xtrain, xtest, ytrain, ytest, sigma2, kernel = rbf):
+    K = make_k(xtrain, sigma2, kernel)
+    k = np.zeros((1, ytrain.shape[0]))
+    Kinv = np.linalg.inv(K)
+
+    mu = np.zeros(ytest.shape[0])
+    var = np.zeros(ytest.shape[0])
+    for i in range(ytest.shape[0]):
+        for j in range(ytrain.shape[0]):
+            k[0][j] = kernel(xtrain[j], xtest[i])
+        s = kernel(xtest[i], xtest[i]) + sigma2
+        mu[i] = (k.dot(Kinv).dot(ytrain))[0][0]
+        var[i] = (s - k.dot(Kinv).dot(k.T))[0][0]
+    return mu, var
