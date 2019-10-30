471ab50ffb7abe8e4eac3020ef2f43956218c941
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-10-30 09:31:51 +0900

    ガウス過程回帰のplot

--- GaussianProcessAndMachineLearning/main3.py
+++ GaussianProcessAndMachineLearning/main3.py
@@ -15,9 +15,9 @@ def kernel_plot(kernels, plot_k):
     
     for i, kernel in enumerate(kernels):
         if (i == 0):
-            k = make_k(x2, kernel)
+            k = make_k(x2, 0, kernel)
         else:
-            k *= make_k(x2, kernel)
+            k *= make_k(x2, 0, kernel)
     #r = np.reshape(mvnrnd(k), (x.shape[0]))
     for _ in range(5):
         r = np.reshape(np.random.multivariate_normal(np.zeros(k.shape[0]), k), (x.shape[0]))
@@ -30,4 +30,23 @@ def kernel_plot(kernels, plot_k):
             plt.plot(x, r.T)
     plt.show()
 
-kernel_plot([periodic_kernel, exponential_kernel], False)
+def gaussian_process_plot():
+    xtrain = np.linspace(-5, 5, 11)
+    xtrain = xtrain.reshape(xtrain.shape[0], 1)
+    noise_var = 0.1
+    ytrain = np.sin(xtrain) + np.random.normal(0, 0.1, (xtrain.shape[0], 1))
+
+    xtest = np.linspace(-6, 6, 500)
+    xtest = xtest.reshape(xtest.shape[0], 1)
+    ytest = np.sin(xtest)
+    mu, var = gaussian_process(xtrain, xtest, ytrain, ytest, noise_var, rbf)
+
+    plt.plot(xtest.reshape(xtest.shape[0]), mu)
+    plt.plot(xtest, ytest)
+    plt.fill_between(xtest.reshape(xtest.shape[0]), mu-2*np.sqrt(var), mu+2*np.sqrt(var), color = '#ccccff')
+    plt.scatter(xtrain, ytrain, c='pink')
+
+    plt.show()
+
+#kernel_plot([periodic_kernel, exponential_kernel], False)
+gaussian_process_plot()
