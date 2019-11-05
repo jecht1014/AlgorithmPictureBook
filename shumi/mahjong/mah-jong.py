a38278ce7fdf00cb3cf7e4ee14b883fcdd8a42b6
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-11-05 22:58:03 +0900

    lstmの作成

---
+++ shumi/mahjong/mah-jong.py
@@ -0,0 +1,139 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn
+from torch.autograd import Variable
+from torch.utils.data.dataset import Dataset
+'''
+if torch.cuda.is_available():
+    import torch.cuda as t
+else:
+    import torch as t
+'''
+from torchvision import datasets, models, transforms, utils
+import torchvision.utils as vutils
+
+import re
+import numpy as np
+from numpy.random import normal
+import os
+import random
+import time
+import math
+import matplotlib.pyplot as plt
+import pickle
+
+from mahjong.shanten import Shanten
+from mahjong.tile import TilesConverter
+
+seed = 1
+random.seed(seed)
+torch.manual_seed(seed)
+
+def load_list(file_name):
+    f = open(file_name, 'rb')
+    l = pickle.load(f)
+    return l
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first = True)
+        self.linear = nn.Linear(hidden_dim, output_dim)
+        self.sigmoid = nn.Sigmoid()
+        self.hidden = None
+
+    def init_hidden(self, batch_size):
+        return (torch.zeros(1, batch_size, self.hidden_dim).to(device), torch.zeros(1, batch_size, self.hidden_dim).to(device))
+
+    def forward(self, inputs):
+        lstm_out, self.hidden = self.lstm(inputs, self.hidden)
+        output = self.linear(self.hidden[0])
+        return output
+
+class mydataset(torch.utils.data.Dataset):
+    def __init__(self, data_num, input_data, label, transform = None):
+        self.transform = transform
+        self.data_num = data_num
+        self.data = input_data
+        self.label = label
+
+    def __len__(self):
+        return self.data_num
+
+    def __getitem__(self, idx):
+        out_data = self.data[idx]
+        out_label = self.label[idx]
+
+        if self.transform:
+            out_data = self.transform(out_data)
+
+        return out_data, out_label
+
+train_size = 0.8
+epochs = 1000
+input_dim = 36
+hidden_dim = 64
+output_dim = 35
+batch_size = 64
+
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+sutehai_jikeiretsu_data = load_list('data/mahjong_lstm_inputdata.txt')
+tenpai_jikeiretsu_data = load_list('data/mahjong_lstm_outputlabel.txt')
+
+input_data = rnn.pad_sequence([torch.FloatTensor(i) for i in sutehai_jikeiretsu_data], batch_first=True)
+label_data = [i[len(i)-1] for i in tenpai_jikeiretsu_data]
+label_data = torch.IntTensor(label_data)
+
+train_num = int(label_data.size()[0]*train_size)
+batch_num = math.ceil(train_num / batch_size)
+
+learning_dataset = mydataset(len(input_data), input_data, label_data)
+train_dataset, test_dataset = torch.utils.data.random_split(learning_dataset, [train_num, label_data.size()[0]-train_num])
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+model = LSTM(input_dim, hidden_dim, output_dim).float().to(device)
+loss_function = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters())
+
+def accuracy(output, label):
+    output = output.reshape(output.size()[1], output.size()[2])
+    label = label.reshape(label.size()[1], label.size()[2])
+    b_num = output.size()[0]
+    a_max = torch.argmax(output, dim = 1)
+    print(a_max)
+    print(label[:, a_max])
+    return torch.sum(label[0,a_max]) / b_num
+    
+
+start_time = time.time()
+history = {'loss': []}
+for epoch in range(1, epochs+1):
+    epoch_loss = 0
+    acc = 0
+
+    for data, label in train_dataloader:
+        model.zero_grad()
+
+        data = data.to(device)
+        label = label.to(device)
+        
+        model.hidden = model.init_hidden(len(data))
+
+        output = model(data)
+        loss = loss_function(output, label.float().reshape(1, label.size()[0], label.size()[1]))
+        acc += accuracy(output, label.float().reshape(1, label.size()[0], label.size()[1]))
+
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.cpu().item()
+        break
+    break
+    history['loss'].append(epoch_loss/batch_num)
+    print('epoch:{0:03} time:{1:.1f} loss{2:.3}'.format(epoch, time.time()-start_time, epoch_loss/batch_num))
