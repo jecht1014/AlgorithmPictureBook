0fb8b64fc9c7b6c1e2313bd853fe355efdac3513
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-11-27 23:08:11 +0900

    システマティック法の追加

--- shumi/monte_carlo_method/find_pi.py
+++ shumi/monte_carlo_method/find_pi.py
@@ -1,5 +1,6 @@
 import random
 import math
+import numpy as np
 
 def distance(coordinate1, coordinate2):
     result = 0
@@ -14,5 +15,14 @@ count = 0
 for i in range(monte_carlo_times):
     if (distance([random.random(), random.random()], [0, 0]) <= 1):
         count += 1
-
 print('pi:{0} monte_carlo:{1}'.format(math.pi/4, count/monte_carlo_times))
+
+# システマティック法
+systematic_times = 1000
+count = 0
+suretsu = np.linspace(0, 1, systematic_times)
+for x in suretsu.tolist():
+    for y in suretsu.tolist():
+        if (distance([x, y], [0, 0]) <= 1):
+            count += 1
+print('pi:{0} systematic:{1}'.format(math.pi/4, count/(systematic_times*systematic_times)))
