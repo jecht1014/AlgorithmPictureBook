9873bb15448780bbf6a115b1602b581d3c2ad971
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-12-06 04:38:28 +0900

    1の数をcountsした数が最大になるようなGA

---
+++ shumi/GA/count_1.py
@@ -0,0 +1,12 @@
+import GA
+
+ga = GA.GA([1, 0], 10, 20, 0.03)
+while (True):
+    for i in range(20):
+        ga.one_point_crossover()
+    ga.mutation()
+    ga.fitness = [sum(g) for g in ga.children_gene]
+    print(max(ga.fitness))
+    if (max(ga.fitness) == 10):
+        break
+    ga.roulette_method()
