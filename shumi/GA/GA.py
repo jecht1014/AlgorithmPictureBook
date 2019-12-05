4d58bf7ea8ba65d1b451c2a081f1203a5dd6b5c0
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-12-06 03:44:42 +0900

    突然変異の追加

--- shumi/GA/GA.py
+++ shumi/GA/GA.py
@@ -1,23 +1,35 @@
 import random
+import copy
 
 class GA:
     def generate_gene(self):
         for i in range(self.gene_num):
             self.gene.append(random.choices(self.genotype, k=self.gene_len))
 
-    def __init__(self, genotype: list, gene_len: int, gene_num: int):
+    def __init__(self, genotype: list, gene_len: int, gene_num: int, mutation_rate: float):
         self.genotype = genotype
         self.gene_len = gene_len
         self.gene_num = gene_num
+        self.mutation_rate = mutation_rate
         self.gene = []
+        self.children_gene = []
         self.generate_gene()
 
     def one_point_crossover(self):
-        parent = []
         parent = random.sample(self.gene, 2)
         cut_point = random.randint(1, self.gene_len-1)
         children = [parent[0][:cut_point]+parent[1][cut_point:], parent[1][:cut_point]+parent[0][cut_point:]]
-        return children
+        self.children_gene += children
 
-ga = GA([1, 2, 3, 4], 10, 20)
-print(ga.one_point_crossover())
+    def mutation(self):
+        for i in range(len(self.children_gene)):
+            for j in range(self.gene_len):
+                if (random.random() <= self.mutation_rate):
+                    l = copy.deepcopy(self.genotype)
+                    l.remove(self.children_gene[i][j])
+                    self.children_gene[i][j] = random.choice(l)
+
+ga = GA([1, 2, 3, 4], 10, 20, 0.03)
+for i in range(20):
+    ga.one_point_crossover()
+ga.mutation()
