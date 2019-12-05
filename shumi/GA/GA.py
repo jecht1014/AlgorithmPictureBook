1b390e064734593dfcccaa933dc6edd8641229c0
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-12-06 04:20:45 +0900

    ルーレット方式の追加

--- shumi/GA/GA.py
+++ shumi/GA/GA.py
@@ -13,6 +13,7 @@ class GA:
         self.mutation_rate = mutation_rate
         self.gene = []
         self.children_gene = []
+        self.fitness = None
         self.generate_gene()
 
     def one_point_crossover(self):
@@ -29,7 +30,25 @@ class GA:
                     l.remove(self.children_gene[i][j])
                     self.children_gene[i][j] = random.choice(l)
 
-ga = GA([1, 2, 3, 4], 10, 20, 0.03)
+    def roulette_method(self):
+        self.gene = []
+        for i in range(self.gene_num):
+            f = copy.deepcopy(self.fitness)
+            f_probability = [float(i) / sum(f) for i in f]
+            probability_sum = 0
+            r = random.random()
+            for j in range(len(self.children_gene)):
+                probability_sum += f_probability[j]
+                if (probability_sum <= r):
+                    self.gene.append(copy.deepcopy(self.children_gene[j]))
+                    del self.children_gene[j]
+                    del self.fitness[j]
+                    break
+        self.children_gene = []
+
+ga = GA([1, 0], 10, 20, 0.03)
 for i in range(20):
     ga.one_point_crossover()
 ga.mutation()
+ga.fitness = [sum(g) for g in ga.children_gene]
+ga.roulette_method()
