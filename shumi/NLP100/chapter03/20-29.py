2420705f2f7168b3212e7009bcda75fcff3155a6
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-12-07 10:01:24 +0900

    28まで完了

--- shumi/NLP100/chapter03/20-29.py
+++ shumi/NLP100/chapter03/20-29.py
@@ -1,6 +1,7 @@
 import json
 import gzip
 import re
+import requests
 
 # 20
 filename = 'jawiki-country.json.gz'
@@ -37,6 +38,51 @@ basic_data = re.findall(pattern, country_data['イギリス']['text'])
 basic_data_dict = {}
 for s in basic_data[0][1:].split('\n|'):
     a = s.split(' = ')
+    if a[1][-1] == '\n':
+        a[1] = a[1][:-1]
+    basic_data_dict[a[0]] = a[1]
+    print('({0}, {1})'.format(a[0], a[1]))
+
+# 26
+pattern = '\{\{基礎情報 国\n([\s\S]+?)}}\n'
+basic_data = re.findall(pattern, country_data['イギリス']['text'])
+basic_data_dict = {}
+for s in basic_data[0][1:].split('\n|'):
+    a = s.split(' = ')
+    a[1] = re.sub('\'\'\'|\'\'\'\'\'', '', a[1])
+    if a[1][-1] == '\n':
+        a[1] = a[1][:-1]
+    basic_data_dict[a[0]] = a[1]
+    print('({0}, {1})'.format(a[0], a[1]))
+
+# 27
+pattern = '\{\{基礎情報 国\n([\s\S]+?)}}\n'
+basic_data = re.findall(pattern, country_data['イギリス']['text'])
+basic_data_dict = {}
+for s in basic_data[0][1:].split('\n|'):
+    a = s.split(' = ')
+    a[1] = re.sub('\'\'\'|\'\'\'\'\'', '', a[1])
+    s = re.findall('([^\[]*?)\[\[([^\|]+?)\]\]([^\[]*?)|([^\[]*?)\[\[[^\|]+?\|([^\]\|]+?)\]\]([^\[]*?)|([\s\S]*)', a[1])
+    a[1] = ''.join(l for x in s for l in x)
+    if a[1][-1] == '\n':
+        a[1] = a[1][:-1]
+    basic_data_dict[a[0]] = a[1]
+    print('({0}, {1})'.format(a[0], a[1]))
+
+# 28
+pattern = '\{\{基礎情報 国\n([\s\S]+?)}}\n'
+basic_data = re.findall(pattern, country_data['イギリス']['text'])
+basic_data_dict = {}
+for s in basic_data[0][1:].split('\n|'):
+    a = s.split(' = ')
+    a[1] = re.sub('\'\'\'|\'\'\'\'\'', '', a[1])
+    s = re.findall('([^\[]*?)\[\[([^\|]+?)\]\]([^\[]*?)|([^\[]*?)\[\[[^\|]+?\|([^\]\|]+?)\]\]([^\[]*?)|([\s\S]*)', a[1]) #タグの削除
+    a[1] = ''.join(l for x in s for l in x)
+    a[1] = re.sub('<ref[^>]*?>[\s\S]+?</ref>|<[^>]*?/>', '', a[1]) #タグの削除
+    s = re.findall('([^\{]*?)\{\{[\s\S]*?\|([^\|]*?)\}\}([^\{]*?)|([\s\S]*)', a[1]) #{{}}の削除
+    a[1] = ''.join(l for x in s for l in x)
+    s = re.findall('([^\[]*?)\[\[[\s\S]*?\|([^\|]*?)\]\]([^\[]*?)|([\s\S]*)', a[1]) #[[]]の削除
+    a[1] = ''.join(l for x in s for l in x)
     if a[1][-1] == '\n':
         a[1] = a[1][:-1]
     basic_data_dict[a[0]] = a[1]
