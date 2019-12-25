974154cef5b4d1fddfad6473c4cdf5b5030fef8a
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2019-12-26 04:58:44 +0900

    Fix: 誰も住んでいなかったところをカウントしていたのを修正
    誰も住んでいないところをカウントすると異人種率を算出するのに空き家を含めてしまうため

--- shumi/cell_and_agents/schelling_segregation_model.py
+++ shumi/cell_and_agents/schelling_segregation_model.py
@@ -3,7 +3,7 @@ import copy
 
 black_num = 22
 white_num = 23
-satisfaction_rate = 0.3
+satisfaction_rate = 0.2
 field_size = (8, 8)
 field_cell_num = field_size[0]*field_size[1]
 
@@ -23,16 +23,17 @@ while change_num != 0:
     for x, y in zip(black_and_whete_idx[0], black_and_whete_idx[1]):
         # 8近傍で同じ人種の数のカウント
         same_race_num = 0
-        neighborhood_num = 0
+        interracial_num = 0
         for i in window:
             for j in window:
                 if 0 <= x+i < field_size[0] and 0 <= y+j < field_size[1]:
                     if field[x, y] == field[x+i, y+j]:
                         same_race_num += 1
-                    neighborhood_num += 1
+                    elif field[x, y] != 0:
+                        interracial_num += 1
         
         # 8近傍に同じ人種が住んでいる率がsatisfaction_rate以下なら引っ越す
-        if (same_race_num-1)/neighborhood_num < satisfaction_rate:
+        if (same_race_num-1)/(same_race_num-1+interracial_num) < satisfaction_rate:
             zero_idx = np.where(field == 0)
             change_num = np.random.choice(range(zero_idx[0].shape[0]))
             field[zero_idx[0][change_num], zero_idx[1][change_num]] = field[x, y]
