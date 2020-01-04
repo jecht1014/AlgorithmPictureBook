45aba835a2937bccf78dcd3c4a3c2ad1c7de7280
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2020-01-05 02:41:56 +0900

    Add: コメントの追加

--- StatisticsLecture/data_processing.py
+++ StatisticsLecture/data_processing.py
@@ -4,15 +4,20 @@ import matplotlib.pyplot as plt
 
 class FrequencyDistribution:
     def __init__(self, data: np.ndarray):
+        # 粗データから中央値を出力
         def calc_median(data):
             if (data.shape[0] % 2 == 1):
                 return data[int(data.shape[0]/2)]
             else:
                 return (data[int(data.shape[0]/2-1)]+data[int(data.shape[0]/2)])/2
+        
+        # 粗データが与えられたとき
         if (data.ndim == 1):
-            self.data = data
+            self.data = data # 粗データ
             self.data.sort()
-            self.avg = self.data.mean()
+            self.avg = self.data.mean() # 平均
+            
+            # 四分位点
             self.quartile = [0]*3
             self.quartile[1] = calc_median(self.data)
             if (data.shape[0] % 2 == 1):
@@ -22,7 +27,8 @@ class FrequencyDistribution:
                 self.quartile[0] = calc_median(self.data[:int(data.shape[0]/2)])
                 self.quartile[2] = calc_median(self.data[int(data.shape[0]/2):])
 
-
+    # 粗データから度数分布を出力
+    # sturgesをTrueにするとすとスタージェスの定理に従って階級数を決定
     def coarse2frequency(self, sturges: bool=True, width: int=None):
         if (sturges):
             width = np.ceil((self.data[-1]-self.data[0])/int(np.ceil(1+np.log2(self.data.shape[0]))))
@@ -40,6 +46,7 @@ class FrequencyDistribution:
 
         return (class_value, frequency)
     
+    # 度数分布表を保存
     def plot_frequency(self, class_value, frequency, save_path):
         plt.bar(class_value, frequency, width=class_value[1]-class_value[0], color='blue', edgecolor='cyan', tick_label=class_value, align="center")
         plt.savefig(save_path)
