5432c44b696db900775866c2c4345fce5d889701
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2020-01-05 15:31:11 +0900

    Fix: history保存の位置をmodelの保存と同じ位置に修正

--- shumi/machine_learning/autoencoder_moving_mnist.py
+++ shumi/machine_learning/autoencoder_moving_mnist.py
@@ -1,3 +1,4 @@
+# 通常のLSTMAutoencoder
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -47,16 +48,11 @@ split_line = 9000
 train_data = data[:, :split_line, :]
 test_data = data[:, split_line:, :]
 
-'''
-for i in range(10):
-    plt.imshow(data[i, 0, :, :])
-    plt.show()
-'''
 batch_size = 64
 input_size = 64*64
 hidden_size = 1024
-epochs = 200
-save_path = 'result/moving_mnist_LSTM_autoencoder_h{0}_2'.format(hidden_size)
+epochs = 1000
+save_path = 'result/moving_mnist_LSTM_autoencoder_h{0}'.format(hidden_size)
 os.makedirs(save_path + '/model', exist_ok=True)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 mnist_sample = train_data[:, :2, :].to(device)
@@ -109,11 +105,8 @@ def makeImage(encoder_model, decoder_model, epoch):
     image_tensor = image_tensor_c
     
     plt.imshow(np.transpose(vutils.make_grid(image_tensor, nrow=10, pad_value=200).cpu().numpy(), (1, 2, 0)))
-    plt.savefig(save_path + '/image/train_sample_{0:03}.png'.format(epoch))
+    plt.savefig(save_path + '/image/train_sample_{0:04}.png'.format(epoch))
     plt.close()
-
-makeImage(encoder, decoder, 1)
-print('end')
 
 history = {'loss': []}
 start_time = time.time()
@@ -143,17 +136,20 @@ for epoch in range(1, epochs+1):
         sum_loss += loss.cpu().item()
     makeImage(encoder, decoder, epoch)
     history['loss'].append(sum_loss/len(input_batch))
-    print('epoch:{0:04} time:{1} loss:{2:.3}'.format(epoch, time.time()-start_time, sum_loss/len(input_batch)))
+    print('epoch:{0:04} time:{1} loss:{2:.3}'.format(epoch, int(time.time()-start_time), sum_loss/len(input_batch)))
 
-    # Save Model
+    # Save Model & History
     if epoch % 10 == 0:
-        model_name = save_path+"/model/autoencoder_v{0:03}.pt".format(epoch)
+        model_name = save_path+"/model/autoencoder_v{0:04}.pt".format(epoch)
         torch.save({
             'encoder_model': encoder.state_dict(),
             'decoder_model': decoder.state_dict(),
             'encoder_optimizer': encoder_optimizer.state_dict(),
             'decoder_optimizer': decoder_optimizer.state_dict(),
         }, model_name)
+
+        with open(save_path + '/history.pickle', mode='wb') as f:
+            pickle.dump(history, f)
 
 plt.figure()
 plt.title("Autoencoder Loss During Training")
@@ -162,6 +158,3 @@ plt.xlabel("epochs")
 plt.ylabel("Loss")
 plt.savefig(save_path + '/loss.png')
 plt.close()
-
-with open(save_path + '/history.pickle', mode='wb') as f:
-    pickle.dump(history, f)
