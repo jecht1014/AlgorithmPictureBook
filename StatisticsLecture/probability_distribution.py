14b311d4b3db58eac3d7c34865652bf8c2466db0
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2020-01-08 21:56:44 +0900

    Add: 連続分布用のクラスの追加

--- StatisticsLecture/probability_distribution.py
+++ StatisticsLecture/probability_distribution.py
@@ -32,10 +32,64 @@ class Discrete:
         plt.savefig(save_path)
         plt.close()
 
+class Continuous:
+    def __init__(self, function, x_range: tuple):
+        self.function = function
+        self.x_range = x_range
+
+    # 期待値(平均)
+    def expectation(self, num=10000):
+        x = np.linspace(self.x_range[0], self.x_range[1], num)
+        y = self.function(x) * x
+        e = 0
+        for i in range(num-1):
+            e += (x[i+1]-x[i]) * (y[i]+y[i+1]) / 2
+        return e
+    # 2乗平均
+    def expectation2(self, num=10000):
+        x = np.linspace(self.x_range[0], self.x_range[1], num)
+        y = self.function(x) * x * x
+        e = 0
+        for i in range(num-1):
+            e += (x[i+1]-x[i]) * (y[i]+y[i+1]) / 2
+        return e
+
+    # 分散
+    def var(self):
+        return self.expectation2()-np.power(self.expectation(), 2)
+
+    # 確率関数のプロット
+    def plot_probability_function(self, save_path, num=10000):
+        x = np.linspace(self.x_range[0], self.x_range[1], num)
+        plt.plot(x, self.function(x))
+        plt.savefig(save_path)
+        plt.close()
+
+    # 分布関数のプロット
+    def plot_distribution_function(self, save_path, num=10000):
+        x = np.linspace(self.x_range[0], self.x_range[1], num)
+        y = self.function(x)
+        dy = np.array([0])
+        for i in range(num-1):
+            dy = np.append(dy, dy[-1]+(x[i+1]-x[i]) * (y[i]+y[i+1]) / 2)
+        plt.xlim(self.x_range[0], self.x_range[1])
+        plt.plot(x, dy)
+        plt.savefig(save_path)
+        plt.close()
+
 def bernoulli(p):
     return (np.array([0, 1]), np.array([1-p, p]))
+
+def norm(x: np.ndarray, mu=0, sigma2=1):
+    return (1/np.sqrt(2*np.pi*sigma2)) * np.exp(-np.power(x-mu, 2)/(2*sigma2))
 
 value, probability = bernoulli(0.8)
 discrete = Discrete(value, probability)
 discrete.plot_probability_function('image/bernoulli_probability.png')
 discrete.plot_distribution_function('image/bernoulli_distribution.png')
+
+continuous = Continuous(norm, (-5, 5))
+continuous.plot_probability_function('image/norm_probability.png')
+continuous.plot_distribution_function('image/norm_distribution.png')
+print(continuous.expectation())
+print(continuous.var())
