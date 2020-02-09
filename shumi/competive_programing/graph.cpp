9fc8722804c03476abd312e63a4e69bc5be3f14e
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2020-02-09 12:29:48 +0900

    Add: グラフ用のクラスとそのクラスにベルマンフォード法の追加

--- shumi/competive_programing/graph.cpp
+++ shumi/competive_programing/graph.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define INF 10e12
 
 // graphは隣接表現
 vector<vector<long long>> warshall_floyd(vector<vector<long long>> graph) {
@@ -13,13 +14,58 @@ vector<vector<long long>> warshall_floyd(vector<vector<long long>> graph) {
     return graph;
 }
 
+class Node {
+    public:
+        vector<long long> edge_value;
+        vector<int> next_node;
+        long long value = 0;
+};
+
+class Graph {
+    public:
+        int node_num;
+        vector<Node> node;
+        
+        // 辺の重みを1で初期化
+        Graph(vector<vector<int>> edge) {
+            node_num = edge.size();
+            node = vector<Node>(edge.size());
+            for (int i = 0; i < edge.size(); i++) {
+                node[i].value = i;
+                for (int j = 0; j < edge[i].size(); j++) {
+                    node[i].push_back(edge[i][j]);
+                    node[i].push_back(1);
+                }
+            }
+        }
+
+        // 重み付き辺有
+        Graph(vector<vector<int>> edge, vector<vector<long long>> edge_weight) {
+            node_num = edge.size();
+            node = vector<Node>(edge.size());
+            for (int i = 0; i < edge.size(); i++) {
+                node[i].value = i;
+                for (int j = 0; j < edge[i].size(); j++) {
+                    node[i].push_back(edge[i][j]);
+                    node[i].push_back(edge_weight[i][j]);
+                }
+            }
+        }
+
+        // ベルマンフォード法(あるノードから全てのノードの最短経路探索)
+        vector<long long> bellman_ford(int n) {
+            vector<long long> shortest_path(node_num, INF);
+            shortest_path[n] = 0;
+            for (int _ = 0; _ < node_num-1; _++)
+                for (int i = 0; i < node_num; i++)
+                    for (int j = 0; j < node[i].next_node.size(); j++)
+                        shortest_path[node[i].next_node[j]] = min(shortest_path[node[i].next_node[j]], shortest_path[i]+node[i].edge_value[j]);
+            return shortest_path;
+        }
+};
+
 int main() {
     vector<vector<int>> hen{{1, 2}, {0, 3}, {0}};
-    vector<set<int>> path = ne_path(hen, 0);
-    for (int i = 0; i < path.size(); i++) {
-        for (auto x : path[i]) {
-            cout << x << " ";
-        }
-        cout << endl;
-    }
+    Graph graph(hen);
+    vector<long long> shortest_path = graph.bellman_ford(0);
 }
