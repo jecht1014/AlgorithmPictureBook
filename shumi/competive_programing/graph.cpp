7176d57b2ab2ae66fb478a48d27c71d0f18571bc
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2020-04-20 03:22:33 +0900

    Add: クラスカル法による最小全域木の追加

--- shumi/competive_programing/graph.cpp
+++ shumi/competive_programing/graph.cpp
@@ -155,6 +155,57 @@ vector<vector<edge>> prim(vector<vector<edge>> graph) {
     return minimum_spanning_tree;
 }
 
+// クラスカル法による最小全域木の構築
+struct edge {
+    int u, v;
+    long long cost;
+};
+
+bool comp(const edge& e1, const edge& e2) {
+    return e1.cost < e2.cost;
+}
+
+struct UnionFind {
+    vector<int> par; // par[i]:iの親の番号 根のときはpar[i]=i
+
+    //最初は全てが根であるとして初期化
+    UnionFind(int N) : par(N) {
+        for(int i = 0; i < N; i++) par[i] = i;
+    }
+
+    // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
+    int root(int x) {
+        if (par[x] == x) return x;
+        return par[x] = root(par[x]);
+    }
+
+    // xとyの木を併合
+    void unite(int x, int y) {
+        int rx = root(x);
+        int ry = root(y);
+        if (rx == ry) return;
+        par[rx] = ry;
+    }
+    
+     // 2つのデータx, yが属する木が同じならtrueを返す
+    bool same(int x, int y) {
+        return root(x) == root(y);
+    }
+};
+
+vector<edge> kruskal(vector<edge> es, int vertex_num) {
+    sort(es.begin(), es+es.end(), comp);
+    UnionFind uftree(vertex_num);
+    vector<edge> ans_graph;
+    for (int i = 0; i < es.size(); i++) {
+        if (!uftree.same(es[i].u, es[i].v)) {
+            uftree.unit(es[i].u, es[i].v);
+            ans_graph.push_back(es[i]);
+        }
+    }
+    return ans_graph;
+}
+
 int main() {
     vector<vector<int>> hen{{1, 2}, {0, 3}, {0}};
     Graph graph(hen);
