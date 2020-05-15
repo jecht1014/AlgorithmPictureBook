6524f5448c36f34e08e457a6de0e56e71558b989
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2020-05-15 23:09:13 +0900

    Add: 高速併合アルゴリズムの追加

---
+++ SedgwickAlgorithmC/chapter1/algorithm/quick_union.py
@@ -0,0 +1,16 @@
+# 高速併合アルゴリズム
+class Quick_Find:
+    def __init__(self, N):
+        self.N = N
+        self.connected_componet = [i for i in range(N)]
+
+    def find(self, a):
+        if a == self.connected_componet[a]:
+            return a
+        return self.find(self.connected_componet[a])
+    
+    def union(self, a, b):
+        a_root = self.find(a)
+        b_root = self.find(b)
+        if (a_root != b_root):
+            self.connected_componet[a_root] = b_root
