158ec4dacb359ab8141a34a344fe9ec060053df1
jecht1014 <hayabusa_tukasa1014@yahoo.ne.jp>
2023-02-26 01:13:36 +0900

    [fix] 変数のスコープを変更

--- LinearAlgebra/test/linear_algebra_test.py
+++ LinearAlgebra/test/linear_algebra_test.py
@@ -5,29 +5,28 @@ import sys
 sys.path.append(os.path.abspath(".."))
 from linear_algebra import Matrix
 
+
 # equalのtest
-matrix_list = [[0, 1], [2, 3]]
-not_equal_matrix_list = [[0, 1, 2], [3, 4, 5]]
-
-matrix = Matrix(matrix_list)
-equal_matrix = Matrix(matrix_list)
-not_equal_matrix = Matrix(not_equal_matrix_list)
-
-
 class EqualTestCase(unittest.TestCase):
+    matrix_list = [[0, 1], [2, 3]]
+    not_equal_matrix_list = [[0, 1, 2], [3, 4, 5]]
+
+    matrix = Matrix(matrix_list)
+    equal_matrix = Matrix(matrix_list)
+    not_equal_matrix = Matrix(not_equal_matrix_list)
+
     def test_equal(self):
-        self.assertTrue(matrix.equal(equal_matrix))
+        self.assertTrue(self.matrix.equal(self.equal_matrix))
 
     def test_not_equal(self):
-        self.assertFalse(matrix.equal(not_equal_matrix))
+        self.assertFalse(self.matrix.equal(self.not_equal_matrix))
 
 
 # zero_matrixのtest
-zero_matrix = Matrix([[0, 0], [0, 0], [0, 0]])
-rows = 2
-columns = 3
-
-
 class ZeroMatrixTestCase(unittest.TestCase):
+    zero_matrix = Matrix([[0, 0], [0, 0], [0, 0]])
+    rows = 2
+    columns = 3
+
     def test_zero_matrix(self):
-        self.assertEqual(zero_matrix, Matrix.zero_matrix(rows, columns))
+        self.assertEqual(self.zero_matrix, Matrix.zero_matrix(self.rows, self.columns))
