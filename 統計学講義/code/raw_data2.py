12b2006ddacd2ecf4aba1715fff369ade3e432d2
jecht1014 <hayabusa_tukasa1014@yahoo.ne.jp>
2024-09-01 00:55:24 +0900

    [add] 2次元データ処理用のコードを追加

---
+++ 統計学講義/code/raw_data2.py
@@ -0,0 +1,32 @@
+import raw_data
+
+class RawData2:
+    """
+    2次元の粗データを扱うクラス
+    """
+    
+    def __init__(self, raw_data: list[int|float]):
+        """
+        コンストラクタ
+
+        Parameters
+        ----------
+        raw_data : list[int|float]
+            粗データ
+        """
+        self.raw_data = raw_data
+        self.length = len(raw_data)
+
+    def covariance(self) -> float:
+        """
+        共分散の計算を行う関数
+
+        Returns
+        -------
+        float
+            共分散
+        """
+        
+        x_mean = raw_data.RawData([raw[0] for raw in self.raw_data]).mean()
+        y_mean = raw_data.RawData([raw[1] for raw in self.raw_data]).mean()
+        return sum([(raw[0]-x_mean)*(raw[1]-y_mean) for raw in self.raw_data])/(self.length)
---
+++ 統計学講義/code/test/test_raw_data2.py
@@ -0,0 +1,35 @@
+import unittest
+import csv
+from pathlib import Path
+
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import raw_data2
+
+class TestRawData2(unittest.TestCase):
+    """
+    raw_data2.pyのテスト
+    """
+
+    def setUp(self) -> None:
+        """
+        テスト用のcsvファイルを事前に読み込む
+        """
+        parent = Path(__file__).resolve().parent
+        row_data = []
+
+        # テスト用のcsvファイルの読み込み
+        with open(parent.joinpath('../sample_data/StatData02_1.csv')) as f:
+            reader = csv.reader(f)
+            row_data = [row for row in reader][1:]
+            row_data = [[float(row[1]), float(row[2])] for row in row_data]
+
+        self.raw_data2 = raw_data2.RawData2(row_data)
+
+    def test_convariance(self):
+        """
+        共分散のテスト
+        """
+        self.assertAlmostEqual(self.raw_data2.covariance(), 424.24, places=2)
+
+unittest.main()
